2bc5fc55a71f01e60c128b2cf826e911a33750fb
Nashmeyah <nashalrekabi@gmail.com>
2020-04-17 02:53:33 -0600

    Working on message bug

--- lib/zodiac/api.rb
+++ lib/zodiac/api.rb
@@ -1,10 +1,10 @@
 class Api
     def self.daily_horoscope(zodiac)
         url = "http://horoscope-api.herokuapp.com/horoscope/today/#{zodiac}"
-        response = Net::HTTP.get(URI(url))
-        
+        response = Net::HTTP.get(URI(url)) 
         horoscope_data = JSON.parse(response)
-        instance = Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
+
+       Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
     end
 
     def self.weekly_horoscope(zodiac)
@@ -12,21 +12,21 @@ class Api
         response = Net::HTTP.get(URI(url))
         horoscope_data = JSON.parse(response)
 
-        instance = Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
+         Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
     end
 
     def self.monthly_horoscope(zodiac)
         url = "http://horoscope-api.herokuapp.com/horoscope/month/#{zodiac}"
         response = Net::HTTP.get(URI(url))
         horoscope_data = JSON.parse(response)
-        instance = Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
+         Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
     end
 
     def self.yearly_horoscope(zodiac)
         url = "http://horoscope-api.herokuapp.com/horoscope/year/#{zodiac}"
         response = Net::HTTP.get(URI(url))
         horoscope_data = JSON.parse(response)
-        instance = Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
+         Horoscope.new(year: horoscope_data["year"], month: horoscope_data["month"],week: horoscope_data["week"],date: horoscope_data["date"], zodiac: horoscope_data["sunsign"], horoscope_info: horoscope_data["horoscope"])
     end
 
 end
